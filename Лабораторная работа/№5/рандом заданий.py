84c80cd4f8743f23e8dc8a8592e4e248c3bbeae8
Bobur <m2401867@edu.misis.ru>
2024-12-28 00:48:52 +0300

    Delete Лабораторная работа/№5/рандом заданий.py

--- Лабораторная работа/№5/рандом заданий.py
+++
@@ -1,8 +0,0 @@
-import random
-import math
-random.seed (17)
-x1=random.randint (1,3)
-x2=random.randint (1,28) 
-x3=random.randint (1,28) 
-print (x1, '- номер 1-ого уровня; ',x2,'- номер 2-ого уровня; ',x3, '- номер 2-ого уровня. ')
-
