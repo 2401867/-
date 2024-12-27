7a11cb3b61a8ea01cd9c0c726ca3e014c17f0509
Bobur <m2401867@edu.misis.ru>
2024-12-27 22:21:20 +0300

    Update рандом заданий.py

--- Лабораторная работа/№1/рандом заданий.py
+++ Лабораторная работа/№1/рандом заданий.py
@@ -3,8 +3,8 @@ import math
 random.seed (17)
 x1=random.randint (1,18)
 x2=random.randint (1,11) 
-x3=random.randint (1,9) 
-print (x1, '- номер 1-ого уровня; ',x2,'- номер 2-ого уровня; ',x3,'- номер 3-ого уровня. ')
+x3=random.randint (1,11) 
+print (x1, '- номер 1-ого уровня; ',x2,'- номер 2-ого уровня; ',x3,'- номер 2-ого уровня. ')
 
 
 
