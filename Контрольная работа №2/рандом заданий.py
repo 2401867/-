88cce62953ba015972e9e1c27edc0e86e5825411
Bobur <m2401867@edu.misis.ru>
2024-12-28 02:24:56 +0300

    Delete Контрольная работа №2/рандом заданий.py

--- Контрольная работа №2/рандом заданий.py
+++
@@ -1,7 +0,0 @@
-import random
-import math
-random.seed (3117)
-x1=random.randint (1,33)
-x2=random.randint (1,28) 
-x3=random.randint (1,6) 
-print (x1, '- номер 1-ого уровня; ',x2,'- номер 2-ого уровня; ',x3,'- номер 3-ого уровня. ')
